086ccdb481328ac65c2ebda2261624cc50294ed4
jaquew <eng.jaquesilva.com>
2019-09-10 17:34:57 -0300

    testes

--- src/main.js
+++ src/main.js
@@ -1,2 +1,4 @@
-// const trabalho = WORLDBANK.filter(pais => (pais.countryCode === "PER"))
 console.log(Object.entries(WORLDBANK.BRA.indicators))
+if (WORLDBANK.PER.indicators[0].indicatorCode === "SL.TLF.PART.FE.ZS") {
+    console.log(WORLDBANK.PER.indicators[0].data[2003])
+}
