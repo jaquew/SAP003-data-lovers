61a6b572594d79df9e34733f8af4b186a581937b
jaquew <eng.jaquesilva.com>
2019-09-12 18:08:47 -0300

    data and main linked

--- src/main.js
+++ src/main.js
@@ -1,21 +1,9 @@
-document.getElementById("btn").addEventListener("click",exibeDados)
+document.getElementById("btn").addEventListener("click",coletaDados)
 
-function exibeDados(){
-  const result = document.getElementById("res1")
-  result.innerHTML = "";
-  for (let nomePais in WORLDBANK){
-    indicadores = WORLDBANK[nomePais].indicators;
-    result.innerHTML += nomePais +"<br>"  
-     
-    for (let i in indicadores){
-      if (indicadores[i].indicatorCode === "SL.EMP.SMGT.FE.ZS"){
-        for (let j=2012; j<=2017; j++){
-          result.innerHTML += "<p>"+j +": " +indicadores[i].data[j] + "</p>" 
-        }
-      }
-    
-    }
-  }
+function coletaDados(){
+  let resultado = document.getElementById("res1")
+  resultado.innerHTML = window.data.exibeDado()
+
 }
 
 
