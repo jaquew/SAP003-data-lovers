fe9c0ac473ce81d29e9617a2f66728c1445d4430
jaquew <eng.jaquesilva.com>
2019-09-12 16:44:52 -0300

    first data printed

--- src/main.js
+++ src/main.js
@@ -1,4 +1,31 @@
-console.log(Object.entries(WORLDBANK.BRA.indicators))
-if (WORLDBANK.PER.indicators[0].indicatorCode === "SL.TLF.PART.FE.ZS") {
-    console.log(WORLDBANK.PER.indicators[0].data[2003])
+document.getElementById("btn").addEventListener("click",exibeDados)
+
+function exibeDados(){
+  const result = document.getElementById("res1")
+  result.innerHTML = "";
+  for (let nomePais in WORLDBANK){
+    indicadores = WORLDBANK[nomePais].indicators;
+    result.innerHTML += nomePais +"<br>"  
+     
+    for (let i in indicadores){
+      if (indicadores[i].indicatorCode === "SL.EMP.SMGT.FE.ZS"){
+        for (let j=2012; j<=2017; j++){
+          result.innerHTML += "<p>"+j +": " +indicadores[i].data[j] + "</p>" 
+        }
+      }
+    
+    }
+  }
 }
+
+
+//   let pais = ""
+//   const radio = document.getElementsByName("pais");
+  
+//   for(i = 0; i < radio.length; i++) { 
+//     if(radio[i].checked) {
+//       pais = radio[i].value
+//     }
+//   }
+//   console.log(pais)
+//   const indicadores = (WORLDBANK[pais].indicators)
