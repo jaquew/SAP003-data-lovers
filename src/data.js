b3339e6b0d47ce454aa2b5b67bbc1504c3f10149
jaquew <eng.jaquesilva.com>
2019-09-13 14:49:31 -0300

    eslint

--- src/data.js
+++ src/data.js
@@ -1,26 +1,23 @@
 const exibeDado = (pais) => {
-  let result = ""
+  let result = "";
 
   const indicadores = WORLDBANK[pais].indicators;
-  console.log(indicadores)
   
   result += `<h3>Proporção de mulheres empregadas em cargos executivos de nível sênior e intermediário (%)</h3>${indicadores[0].countryName}<br>`;
      
-  for (let indicador of indicadores){
-    if (indicador.indicatorCode === "SL.EMP.SMGT.FE.ZS"){
-      for (let ano=2012; ano<=2017; ano++){
-        if (indicador.data[ano]===""){
-          result += `<p>${ano}: não há dados </p>`
-        } else{
-          result += `<p>${ano}: ${indicador.data[ano].toFixed(2)} </p>`
+  for (let indicador of indicadores) {
+    if (indicador.indicatorCode === "SL.EMP.SMGT.FE.ZS") {
+      for (let ano=2012; ano<=2017; ano++) {
+        if (indicador.data[ano]==="") {
+          result += `<p>${ano}: não há dados </p>`;
+        } else {
+          result += `<p>${ano}: ${indicador.data[ano].toFixed(2)} </p>`;
         }
       }
     }
   }
-  console.log(typeof result)
   return result;
 };
 
-window.data ={exibeDado: exibeDado}
-
+window.data ={exibeDado: exibeDado};
 
--- src/main.js
+++ src/main.js
@@ -1,14 +1,14 @@
-document.getElementById("btn").addEventListener("click",coletaDados)
+document.getElementById("btn").addEventListener("click", coletaDados);
 
-function coletaDados(){
-  let resultado = document.getElementById("res1")
+function coletaDados() {
+  let resultado = document.getElementById("res1");
   
-  let pais = ""
+  let pais = "";
   const radio = document.getElementsByName("pais");
-  for(let i in radio) { 
-    if(radio[i].checked) {
-      pais = radio[i].value
+  for (let i in radio) { 
+    if (radio[i].checked) {
+      pais = radio[i].value;
     }
   }
-  resultado.innerHTML = window.data.exibeDado(pais)
+  resultado.innerHTML = window.data.exibeDado(pais);
 }
