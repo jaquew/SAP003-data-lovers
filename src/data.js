fdf894659e1e0f83bbccec85cbc5a214ef77cf2c
Jaqueline Wanzeler <eng.jaquesilva@gmail.com>
2019-09-16 10:53:03 -0300

    country filter

--- src/data.js
+++ src/data.js
@@ -1,9 +1,34 @@
-const exibeDado = (pais) => {
-  let result = "";
+let indicadores = "";
+let result = "";
+// let pais = "";
+window.addEventListener("load",entrada)
+function entrada() {
+  for (let pais in WORLDBANK){    
+    indicadores = WORLDBANK[pais].indicators
+    for (let indicador of indicadores) {
+      if (indicador.indicatorCode === "SL.EMP.SMGT.FE.ZS") {
+        document.getElementById("nomeDado").innerHTML= `<h3>${indicador.indicatorName}</h3>`
+        result += indicadores[0].countryName
+        for (let ano=2012; ano<=2017; ano++) {
+          if (indicador.data[ano]==="") {
+            result += `<p>${ano}: não há dados </p>`;
+          } else {
+            result += `<p>${ano}: ${indicador.data[ano].toFixed(2)} </p>`;
+          }
+        }
+      }
+    
+  }
+  resultado.innerHTML = result
+}
+}
 
-  const indicadores = WORLDBANK[pais].indicators;
-  
-  result += `<h3>Proporção de mulheres empregadas em cargos executivos de nível sênior e intermediário (%)</h3>${indicadores[0].countryName}<br>`;
+// FILTRO de pais
+// tentar usar o filter()
+const exibeDado = (pais) => {
+  result = "";
+  indicadores = WORLDBANK[pais].indicators
+  result += `${indicadores[0].countryName}<br>`;
      
   for (let indicador of indicadores) {
     if (indicador.indicatorCode === "SL.EMP.SMGT.FE.ZS") {
@@ -17,6 +42,5 @@ const exibeDado = (pais) => {
     }
   }
   return result;
-};
-
+}
 window.data ={exibeDado: exibeDado};
--- src/main.js
+++ src/main.js
@@ -1,14 +1,22 @@
 document.getElementById("btn").addEventListener("click", coletaDados);
 
-function coletaDados() {
-  let resultado = document.getElementById("res1");
-  
-  let pais = "";
+for (let pais in WORLDBANK){
+  opCountry = document.getElementById("countries");
+  opCountry.innerHTML+=`<input type="radio" name="pais" value = "${pais}">${WORLDBANK[pais].indicators[0].countryName}`;  
+}
+
+let resultado = document.getElementById("res1");
+
+function coletaDados() { 
   const radio = document.getElementsByName("pais");
   for (let i in radio) { 
     if (radio[i].checked) {
       pais = radio[i].value;
     }
   }
-  resultado.innerHTML = window.data.exibeDado(pais);
+  if (radio.checked==false){
+    alert("Selecione um país")
+  } else {
+    resultado.innerHTML = window.data.exibeDado(pais);
+  }
 }
