927a9b62dad7cadccdc9dba6e371467ce5f1290d
tiemimaeda <priscila.maeda@yahoo.com.br>
2019-09-17 18:03:22 -0300

    Filtro de indicadores

--- src/data.js
+++ src/data.js
@@ -1,24 +1,11 @@
-exibeDado = (pais) => {
-  let result = "";
-
-  const indicadores = WORLDBANK[pais].indicators;
-  
-  result += `${indicadores[0].countryName}<br>`;
-  
-  indicadores.map(indicador => {
+// Seleciona os indicadores que contém SL e mujeres no nome do indicador
+exibeDado = (indicadores) => {
+  return indicadores.filter(indicador => {
     const indNome = indicador.indicatorName;
     if (indicador.indicatorCode.slice(0, 2) === "SL" && indNome.search("mujeres")!==-1) {
-      result += indNome;
-      for (let ano=2016; ano<=2017; ano++) {
-        if (indicador.data[ano]==="") {
-          result += `<p>${ano}: não há dados </p>`;
-        } else {
-          result += `<p>${ano}: ${indicador.data[ano].toFixed(2)} </p>`;
-        }
-      }
+      return indicador;
     }
   });
-  return result;
 };
 
 window.data ={
